a1f6b3374842c3348ccdd1556a474db17f3698f9
kathrynzaro <zarokathryn@gmail.com>
2022-08-03 14:57:17 -0700

    adds post route to authors

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -9,14 +9,14 @@ describe('authors routes', () => {
     return setup(pool);
   });
 
-  it('get /authors should return a list of authors', async () => {
+  it('#GET /authors should return a list of authors', async () => {
     const res = await request(app).get('/authors');
     expect(res.body.length).toEqual(6);
     const daphne = res.body.find((author) => author.id === '1');
     expect(daphne).toHaveProperty('name', 'Daphne du Maurier');
   });
 
-  it('get /:id should return an author with books', async () => {
+  it('#GET /:id should return an author with books', async () => {
     const res = await request(app).get('/authors/1');
     expect(res.status).toBe(200);
     expect(res.body).toEqual({
@@ -28,6 +28,14 @@ describe('authors routes', () => {
         { id: 1, title: 'Rebecca', year: 1938 },
       ],
     });
+  });
+
+  it('#POST /authors should create a new author', async () => {
+    const res = await request(app).post('/authors').send({ name: 'Shel Silverstein', dob: 'September 25, 1930', pob: 'Chicago, Illinois, United States' });
+    expect(res.status).toBe(200);
+    expect(res.body.name).toBe('Shel Silverstein');
+    expect(res.body.dob).toBe('September 25, 1930');
+    expect(res.body.pob).toBe('Chicago, Illinois, United States');
   });
 
   afterAll(() => {
