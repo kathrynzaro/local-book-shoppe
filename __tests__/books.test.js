9a10e9fe268566c49e0697de35b27812f0b6e446
kathrynzaro <zarokathryn@gmail.com>
2022-08-03 10:31:51 -0700

    get books/:id display book detail and authors

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -8,12 +8,24 @@ describe('books routes', () => {
     return setup(pool);
   });
 
-  it(' get /books should return a list of books', async () => {
+  it('get /books should return a list of books', async () => {
     const res = await request(app).get('/books');
     expect(res.body.length).toEqual(8);
     const rebecca = res.body.find((book) => book.id === '1');
     expect(rebecca).toHaveProperty('title', 'Rebecca');
     expect(rebecca).toHaveProperty('year', 1938);
+  });
+  
+  it('get /:id should return a book with authors', async () => {
+    const res = await request(app).get('/books/1');
+    expect(res.body).toEqual({
+      id: '1',
+      title: 'Rebecca',
+      year: 1938,
+      authors: [
+        { id: 1, name: 'Daphne du Maurier', dob: 'May 13, 1907', pob: 'London, England' },
+      ],
+    });
   });
 
   afterAll(() => {
