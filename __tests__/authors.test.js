adb3d144bb33399c45bc9b72cd33f9299843fdcc
kathrynzaro <zarokathryn@gmail.com>
2022-08-02 17:16:55 -0700

    /authors test, controller, model

---
+++ __tests__/authors.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('authors routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('get /authors should return a list of authors', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.body.length).toEqual(6);
+    const daphne = res.body.find((author) => author.id === '1');
+    expect(daphne).toHaveProperty('name', 'Daphne du Maurier');
+    expect(daphne).toHaveProperty('dob', 'May 13, 1907');
+    expect(daphne).toHaveProperty('pob', 'London, England');
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
